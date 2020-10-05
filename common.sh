8e483427e4d16bdee906aa547ae7d731fd6bdcc3
Simon Wright <simon@pushface.org>
2020-10-05 13:18:25 +0100

    Building for GNAT CE 2020.
      * common.sh: Update PATH, PREFIX, GCC_PATH.
        Define CFLAGS, CXXFLAGS to use -specs=ce2020-specs.
      * ce2020-specs: it turns out that GNAT has never included libgcc_s
          in links (needed for binutils, gdb builds).

--- common.sh
+++ common.sh
@@ -6,16 +6,20 @@
 # source tree already had these versions installed via
 # contrib/download_prerequisites.
 
-PATH=/Volumes/Miscellaneous/tmp/opt/gcc-10.1.0/bin:$PATH
+PATH=/opt/gnat-ce-2020/bin:$PATH
 TOP=/Volumes/Miscellaneous
-PREFIX=$TOP/arm/gcc-10.1.0
+PREFIX=$TOP/arm/gnat-ce-2020
 BUILD=x86_64-apple-darwin15
 
 SRC_PATH=$TOP/tmp
 BINUTILS_PATH=$SRC_PATH/binutils-2.34
-GCC_PATH=$SRC_PATH/gcc-10.1.0
+GCC_PATH=$SRC_PATH/gcc-8-2020-20200429-19AA8-src
 NEWLIB_PATH=$SRC_PATH/newlib-3.3.0
 GDB_PATH=$SRC_PATH/gdb-9.1
+
+script_loc=`cd $(dirname ${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}) && pwd -P`
+export CFLAGS=-specs=$script_loc/ce2020-specs
+export CXXFLAGS=-specs=$script_loc/ce2020-specs
 
 GCC_STAGE1_LDFLAGS="-static-libstdc++ -static-libgcc -Wl,-headerpad_max_install_names"
 
